3fc496f3f910100030ab2b3bcb05ba7a7d3a8aa5
AleksandrButakov <anbngm@gmail.com>
2023-09-18 17:54:46 +0500

    chande class

--- src/test/java/tests/naumen/MainPageApiTests.java
+++ src/test/java/tests/naumen/MainPageApiTests.java
@@ -1,142 +1,143 @@
 package tests.naumen;
 
-import models.lombok.ApiResponseLombokModel;
-import org.assertj.core.api.AssertionsForClassTypes;
+import io.restassured.response.Response;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
 import static helpers.CustomAllureListener.*;
 import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
+import static org.assertj.core.api.Assertions.assertThat;
 
 public class MainPageApiTests {
 
     @Test
     @Tag("api")
     void checkMainPage() {
-        ApiResponseLombokModel response = step("Make get request", () ->
+        Response response = step("Make get request", () ->
                 given()
                         .filter(withCustomTemplates())
                         .log().uri()
                         .get("https://www.naumen.ru/")
                         .then()
                         .log().status()
-                        .extract().as(ApiResponseLombokModel.class));
+                        .extract().response());
 
-        step("Verify response", () ->
-                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
+        step("Verification statusCode", () ->
+                assertThat(response.statusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkProducts() {
-        ApiResponseLombokModel response = step("Make get request", () ->
+        Response response = step("Make get request", () ->
                 given()
                         .filter(withCustomTemplates())
                         .log().uri()
                         .get("https://www.naumen.ru/products/")
                         .then()
                         .log().status()
-                        .extract().as(ApiResponseLombokModel.class));
+                        .extract().response());
 
-        step("Verify response", () ->
-                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
+        step("Verification statusCode", () ->
+                assertThat(response.statusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkSupport() {
-        ApiResponseLombokModel response = step("Make get request", () ->
+        Response response = step("Make get request", () ->
                 given()
                         .filter(withCustomTemplates())
                         .log().uri()
                         .get("https://www.naumen.ru/support/")
                         .then()
                         .log().status()
-                        .extract().as(ApiResponseLombokModel.class));
+                        .extract().response());
 
-        step("Verify response", () ->
-                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
+        step("Verification statusCode", () ->
+                assertThat(response.statusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkClients() {
-        ApiResponseLombokModel response = step("Make get request", () ->
+        Response response = step("Make get request", () ->
                 given()
                         .filter(withCustomTemplates())
                         .log().uri()
                         .get("https://www.naumen.ru/clients/")
                         .then()
                         .log().status()
-                        .extract().as(ApiResponseLombokModel.class));
+                        .extract().response());
 
-        step("Verify response", () ->
-                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
+        step("Verification statusCode", () ->
+                assertThat(response.statusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkPartners() {
-        ApiResponseLombokModel response = step("Make get request", () ->
+        Response response = step("Make get request", () ->
                 given()
                         .filter(withCustomTemplates())
                         .log().uri()
                         .get("https://www.naumen.ru/partners/")
                         .then()
                         .log().status()
-                        .extract().as(ApiResponseLombokModel.class));
+                        .extract().response());
 
-        step("Verify response", () ->
-                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
+        step("Verification statusCode", () ->
+                assertThat(response.statusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkCompany() {
-        ApiResponseLombokModel response = step("Make get request", () ->
+        Response response = step("Make get request", () ->
                 given()
                         .filter(withCustomTemplates())
                         .log().uri()
                         .get("https://www.naumen.ru/company/")
                         .then()
                         .log().status()
-                        .extract().as(ApiResponseLombokModel.class));
+                        .extract().response());
 
-        step("Verify response", () ->
-                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
+        step("Verification statusCode", () ->
+                assertThat(response.statusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkNews() {
-        ApiResponseLombokModel response = step("Make get request", () ->
+        Response response = step("Make get request", () ->
                 given()
                         .filter(withCustomTemplates())
                         .log().uri()
                         .get("https://www.naumen.ru/events/news/")
                         .then()
                         .log().status()
-                        .extract().as(ApiResponseLombokModel.class));
+                        .extract().response());
 
-        step("Verify response", () ->
-                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
+        step("Verification statusCode", () ->
+                assertThat(response.statusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkCareer() {
-        ApiResponseLombokModel response = step("Make get request", () ->
+        Response response = step("Make get request", () ->
                 given()
                         .filter(withCustomTemplates())
                         .log().uri()
                         .get("https://www.naumen.ru/events/news/")
                         .then()
                         .log().status()
-                        .extract().as(ApiResponseLombokModel.class));
+                        .statusCode(200)
+                        .extract().response());
 
-        step("Verify response", () ->
-                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
+        step("Verification statusCode", () ->
+                assertThat(response.statusCode()).isEqualTo(200));
     }
 
 }
