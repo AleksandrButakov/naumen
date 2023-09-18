5e332125ba96b44dffa3c43f3a100c33f25e4b2d
AleksandrButakov <anbngm@gmail.com>
2023-09-18 16:05:04 +0500

    feat: add lombok in api tests

--- src/test/java/tests/naumen/MainPageApiTests.java
+++ src/test/java/tests/naumen/MainPageApiTests.java
@@ -1,107 +1,151 @@
 package tests.naumen;
 
+import helpers.CustomAllureListener;
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import models.lombok.ApiResponseLombokModel;
+import models.lombok.LoginBodyLombokModel;
+import models.pojo.LoginResponsePojoModel;
+import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
+import org.openqa.selenium.devtools.v85.fetch.model.AuthChallengeResponse;
 
 import static helpers.CustomAllureListener.*;
+import static io.qameta.allure.Allure.step;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.MatcherAssert.assertThat;
 
 public class MainPageApiTests {
 
     @Test
     @Tag("api")
     void checkMainPage() {
-        given()
-                .filter(withCustomTemplates())
-                .log().uri()
-                .get("https://www.naumen.ru/")
-                .then()
-                .log().status()
-                .statusCode(200);
+        ApiResponseLombokModel response = step("Make get request", () ->
+                given()
+                        .filter(withCustomTemplates())
+                        .log().uri()
+                        .get("https://www.naumen.ru/")
+                        .then()
+                        .log().status()
+                        .extract().as(ApiResponseLombokModel.class));
+
+        step("Verify response", () ->
+                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkProducts() {
-        given()
-                .filter(withCustomTemplates())
-                .log().uri()
-                .get("https://www.naumen.ru/products/")
-                .then()
-                .log().status()
-                .statusCode(200);
+        ApiResponseLombokModel response = step("Make get request", () ->
+                given()
+                        .filter(withCustomTemplates())
+                        .log().uri()
+                        .get("https://www.naumen.ru/products/")
+                        .then()
+                        .log().status()
+                        .extract().as(ApiResponseLombokModel.class));
+
+        step("Verify response", () ->
+                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkSupport() {
-        given()
-                .filter(withCustomTemplates())
-                .log().uri()
-                .get("https://www.naumen.ru/support/")
-                .then()
-                .log().status()
-                .statusCode(200);
+        ApiResponseLombokModel response = step("Make get request", () ->
+                given()
+                        .filter(withCustomTemplates())
+                        .log().uri()
+                        .get("https://www.naumen.ru/support/")
+                        .then()
+                        .log().status()
+                        .extract().as(ApiResponseLombokModel.class));
+
+        step("Verify response", () ->
+                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkClients() {
-        given()
-                .filter(withCustomTemplates())
-                .log().uri()
-                .get("https://www.naumen.ru/clients/")
-                .then()
-                .log().status()
-                .statusCode(200);
+        ApiResponseLombokModel response = step("Make get request", () ->
+                given()
+                        .filter(withCustomTemplates())
+                        .log().uri()
+                        .get("https://www.naumen.ru/clients/")
+                        .then()
+                        .log().status()
+                        .extract().as(ApiResponseLombokModel.class));
+
+        step("Verify response", () ->
+                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkPartners() {
-        given()
-                .filter(withCustomTemplates())
-                .log().uri()
-                .get("https://www.naumen.ru/partners/")
-                .then()
-                .log().status()
-                .statusCode(200);
+        ApiResponseLombokModel response = step("Make get request", () ->
+                given()
+                        .filter(withCustomTemplates())
+                        .log().uri()
+                        .get("https://www.naumen.ru/partners/")
+                        .then()
+                        .log().status()
+                        .extract().as(ApiResponseLombokModel.class));
+
+        step("Verify response", () ->
+                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkCompany() {
-        given()
-                .filter(withCustomTemplates())
-                .log().uri()
-                .get("https://www.naumen.ru/company/")
-                .then()
-                .log().status()
-                .statusCode(200);
+        ApiResponseLombokModel response = step("Make get request", () ->
+                given()
+                        .filter(withCustomTemplates())
+                        .log().uri()
+                        .get("https://www.naumen.ru/company/")
+                        .then()
+                        .log().status()
+                        .extract().as(ApiResponseLombokModel.class));
+
+        step("Verify response", () ->
+                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkNews() {
-        given()
-                .filter(withCustomTemplates())
-                .log().uri()
-                .get("https://www.naumen.ru/events/news/")
-                .then()
-                .log().status()
-                .statusCode(200);
+        ApiResponseLombokModel response = step("Make get request", () ->
+                given()
+                        .filter(withCustomTemplates())
+                        .log().uri()
+                        .get("https://www.naumen.ru/events/news/")
+                        .then()
+                        .log().status()
+                        .extract().as(ApiResponseLombokModel.class));
+
+        step("Verify response", () ->
+                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
     }
 
     @Test
     @Tag("api")
     void checkMainPageLinkCareer() {
-        given()
-                .filter(withCustomTemplates())
-                .log().uri()
-                .get("https://www.naumen.ru/career/")
-                .then()
-                .log().status()
-                .statusCode(200);
+        ApiResponseLombokModel response = step("Make get request", () ->
+                given()
+                        .filter(withCustomTemplates())
+                        .log().uri()
+                        .get("https://www.naumen.ru/events/news/")
+                        .then()
+                        .log().status()
+                        .extract().as(ApiResponseLombokModel.class));
+
+        step("Verify response", () ->
+                AssertionsForClassTypes.assertThat(response.getStatusCode()).isEqualTo(200));
     }
 
 }
